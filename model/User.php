af58f48176136b66b771f8faa8d6ccb2e661d86a
Lancel0tt <anto25.vecchio@gmail.com>
2024-04-10 11:42:02 +0200

    implementazione iniziale

--- model/User.php
+++ model/User.php
@@ -10,4 +10,12 @@ class UserModel extends Database {
     public function addUser($username, $email, $pw) {
         $this->executeStatement("INSERT INTO users (username, email, pass_hash) VALUES (?, ?, ?)", ["sss", $username, $email, password_hash($pw, PASSWORD_DEFAULT)]);
     }
+
+    public function login($email, $pw) {
+        $usrarr = $this->select("SELECT id, pass_hash FROM users WHERE email=?", ["s", $email]);
+        if (isset($usrarr[0]) && password_verify($pw, $usrarr[0]["pass_hash"])) {
+            $_SESSION["email"] = $usrarr[0]["id"];
+            header("Location: /");
+        }
+    }
 }
---
+++ views/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        $model->login($email, $password);
+    }
+    ?>
+    <form action="/login" method="POST">
+        <input type="text" name="email" placeholder="E-Mail" required><br>
+        <label for="password">Password</label><br>
+        <input type="password" name="password" required><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
