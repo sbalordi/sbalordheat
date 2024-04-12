80409ad13bc9f05d44c3c3413a105e83f6db06ff
Davide Guerrazzi <g.davide2005@gmail.com>
2024-04-12 10:12:39 +0200

    Rewrite login

--- views/login.php
+++ views/login.php
@@ -12,14 +12,13 @@ session_start();
   </head>
   <body class="d-flex">
     <?php include __DIR__ . "/../style_components/navbar.php"; ?>
-    <div class="container w-50 mx-auto my-5">
+    <div class="container w-50 mx-auto mt-6">
       <form action="/login" method="POST">
           <label for="email">E-Mail</label><br>
           <input type="email" class="form-control" name="email" placeholder="name@example.com" required><br>
           <label for="password">Password</label><br>
           <input type="password" class="form-control" name="password" required><br>
-          <input type="submit" class="btn btn-primary" value="Login">
-          <p>Non disponete di un account? <a href="/register">Registratevi</a></p>
+          <input type="submit" class="btn btn-primary" value="Login"><p>Non disponete di un account? <a href="/register">Registratevi</a></p>
       </form>
       <?php
       $model = new UserModel();
