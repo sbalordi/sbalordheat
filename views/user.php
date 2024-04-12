e0a157fb0072ac07590a0c18da3c0f68a2971907
Davide Guerrazzi <g.davide2005@gmail.com>
2024-04-12 10:45:25 +0200

    add navbar to user page

--- views/user.php
+++ views/user.php
@@ -7,26 +7,26 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <title>Document</title>
     <?php include __DIR__ . "/../inc/style.php"; ?>
-</head>
-<body>
+  </head>
+  <body>
     <?php
-        require __DIR__ . "/../controller/api/UserController.php";
+    include __DIR__ . "/../style_components/navbar.php";
 
-        $id = $_SESSION["id"];
+    require __DIR__ . "/../controller/api/UserController.php";
 
-        $model = new UserModel;
+    $id = $_SESSION["id"];
+    $model = new UserModel;
+    $user = $model->getUser($id);
 
-        $user = $model->getUser($id);
-
-        if ($user) {
-            echo "<p>".$user["username"]."</p>";
-            echo "<p>".$user["email"]."</p>";
-        } else {
-            require __DIR__ . "/404.php";
-        }
+    if ($user) {
+      echo "<p>".$user["username"]."</p>";
+      echo "<p>".$user["email"]."</p>";
+    } else {
+      require __DIR__ . "/404.php";
+    }
     ?>
-</body>
+  </body>
 </html>
