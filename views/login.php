64fa1bf0f369394afd5e9ae8da99f6ac80783d39
Davide Guerrazzi <g.davide2005@gmail.com>
2024-04-12 10:02:24 +0200

    Rewrite login

--- views/login.php
+++ views/login.php
@@ -12,7 +12,7 @@ session_start();
 </head>
 <body>
     <?php include __DIR__ . "/../style_components/navbar.php"; ?>
-    <div class="w-50 m-auto">
+    <div class="w-50 mx-auto my-5">
     <form action="/login" method="POST">
         <label for="email">E-Mail</label><br>
         <input type="email" class="form-control" name="email" placeholder="name@example.com" required><br>
