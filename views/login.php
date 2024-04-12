7baf0364f27d70379a388bd516a49fc7fd92bab4
Davide Guerrazzi <g.davide2005@gmail.com>
2024-04-12 10:09:17 +0200

    Rewrite login

--- views/login.php
+++ views/login.php
@@ -4,41 +4,41 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <title>Login</title>
     <?php 
     include __DIR__ . "/../inc/style.php"; 
     ?>
-</head>
-<body>
+  </head>
+  <body class="d-flex">
     <?php include __DIR__ . "/../style_components/navbar.php"; ?>
     <div class="container w-50 mx-auto my-5">
-    <form action="/login" method="POST">
-        <label for="email">E-Mail</label><br>
-        <input type="email" class="form-control" name="email" placeholder="name@example.com" required><br>
-        <label for="password">Password</label><br>
-        <input type="password" class="form-control" name="password" required><br>
-        <input type="submit" class="btn btn-primary" value="Login">
-    </form>
-    <?php
-    $model = new UserModel();
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        try{
-        $email = $_POST["email"];
-        $password = $_POST["password"];
+      <form action="/login" method="POST">
+          <label for="email">E-Mail</label><br>
+          <input type="email" class="form-control" name="email" placeholder="name@example.com" required><br>
+          <label for="password">Password</label><br>
+          <input type="password" class="form-control" name="password" required><br>
+          <input type="submit" class="btn btn-primary" value="Login">
+      </form>
+      <?php
+      $model = new UserModel();
+      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        try {
+          $email = $_POST["email"];
+          $password = $_POST["password"];
 
-        $id = $model->login($email, $password);
-        if ($id != null) {
-            $_SESSION["id"] = $id;
-            header( $header = "Location: /");
-        }
+          $id = $model->login($email, $password);
+          if ($id != null) {
+              $_SESSION["id"] = $id;
+              header( $header = "Location: /");
+          }
         } catch (Exception $e) {
-            echo "<div class='alert alert-danger' role='alert'>Errore: " . $e->getMessage() . "</div>";
+              echo "<div class='alert alert-danger' role='alert'>Errore: " . $e->getMessage() . "</div>";
         }
-    }
-    ?>
+      }
+      ?>
     </div>
-</body>
+  </body>
 </html>
 <?php
 ob_end_flush();
