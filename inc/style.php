9ad4086821407247ce8cebf445fdb11f35e0be09
Davide Guerrazzi <g.davide2005@gmail.com>
2024-04-13 00:44:14 +0200

    cover style

--- inc/style.php
+++ inc/style.php
@@ -60,5 +60,10 @@
         .brand {
             font-size: 3rem;
         }
+
+        .mainBody {
+          background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 30%, rgba(0,0,0,0.90) 50%, rgba(0,0,0,0.40) 100%), url("https://static.ohga.it/wp-content/uploads/sites/24/2019/06/iStock-476697500.jpg");
+          background-size: cover;
+        }
     }
 </style>
