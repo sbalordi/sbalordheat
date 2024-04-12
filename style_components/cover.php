8a02d429731fd1567b4231cbc08f578e3da95019
Davide Guerrazzi <g.davide2005@gmail.com>
2024-04-13 00:13:14 +0200

    cover style

--- style_components/cover.php
+++ style_components/cover.php
@@ -1,6 +1,6 @@
         <header class="mb-auto">
           <div>
-            <h3 class="float-md-start mb-0 nav-brand">SbalordHEAT</h3>
+            <h3 class="float-md-start mb-0 nav-brand text-light">SbalordHEAT</h3>
             <nav class="nav nav-masthead justify-content-center float-md-end">
               <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="/">Home</a>
               <a class="nav-link fw-bold py-1 px-0" href="/about">About us</a>
