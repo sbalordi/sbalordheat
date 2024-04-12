a296bf31aeb8e0406e4b6d97adfc1074d9034e9d
Davide Guerrazzi <g.davide2005@gmail.com>
2024-04-12 23:40:55 +0200

    cover style

--- inc/style.php
+++ inc/style.php
@@ -11,7 +11,7 @@
     }
 
     .mainBody {
-        background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 30%, rgba(0,0,0,0.60) 50%, rgba(0,0,0,0.40) 100%), url("https://static.ohga.it/wp-content/uploads/sites/24/2019/06/iStock-476697500.jpg");
+        background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 5%, rgba(0,0,0,0.60) 50%, rgba(0,0,0,0.40) 100%), url("https://static.ohga.it/wp-content/uploads/sites/24/2019/06/iStock-476697500.jpg");
         background-size: cover;
     }
     
