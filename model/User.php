1b5b4e9df842510ccd77d5f784fa82fc11e502df
Davide Guerrazzi <49415276+crazy-catzzz@users.noreply.github.com>
2024-04-16 17:08:24 +0200

    Jeb (#8)
    * I broke everything
    
    * I love deiv
    
    * fix user dashboard
    
    ---------
    
    Co-authored-by: Lancel0tt <anto25.vecchio@gmail.com>

--- model/User.php
+++ model/User.php
@@ -3,12 +3,12 @@
 
 class UserModel extends Database {
     public function getUser($id) {
-        $usrarr = $this->select("SELECT id, username, email FROM users WHERE id=?", ["i", $id]);
+        $usrarr = $this->select("SELECT id, username, email, nome, cognome, azienda FROM users WHERE id=?", ["i", $id]);
         return isset($usrarr[0])? $usrarr[0] : null;
     }
 
-    public function addUser($username, $email, $pw) {
-        $this->executeStatement("INSERT INTO users (username, email, pass_hash) VALUES (?, ?, ?)", ["sss", $username, $email, password_hash($pw, PASSWORD_DEFAULT)]);
+    public function addUser($username, $email, $pw, $nome, $cognome, $azienda){
+        $this->executeStatement("INSERT INTO users (username, email, pass_hash, nome, cognome, azienda) VALUES (?, ?, ?, ?, ?, ?)", ["ssssss", $username, $email, password_hash($pw, PASSWORD_DEFAULT), $nome, $cognome, $azienda]);
     }
 
     public function login($email, $pw) {
@@ -21,4 +21,13 @@ class UserModel extends Database {
         }
         return null;
     }
+
+    public function logout() {
+        if (session_id() != '' || isset($_SESSION)) {
+            session_unset();
+            session_destroy();
+            header("Location: /login");
+        }
+    }
+
 }
