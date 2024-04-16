46382e7e5a4c914e978e0d645957b073fa0f3261
Lancel0tt <anto25.vecchio@gmail.com>
2024-04-16 18:45:35 +0200

    idk

--- style_components/cover.php
+++ style_components/cover.php
@@ -3,7 +3,6 @@
             <h3 class="float-md-start mb-0 nav-brand text-light">SbalordHEAT</h3>
             <nav class="nav nav-masthead justify-content-center float-md-end">
               <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="/">Home</a>
-              <a class="nav-link fw-bold py-1 px-0" href="/about">About us</a>
               <a class="nav-link fw-bold py-1 px-0" href="/contacts">Contacts</a>
               <a class="nav-link fw-bold py-1 px-0" href="/user">Dashboard</a>
             </nav>
